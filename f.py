8880f62778b056c9ab5aa9bf9710b42504693815
DottyyDev <91227484+DottyyDev@users.noreply.github.com>
2021-11-30 09:11:51 -0500

    Create f.py
    i need to commit

---
+++ f.py
@@ -0,0 +1,10 @@
+import random
+"""
+most of the thing in here represent human behaviors that occur in a park, there could be a lot more and feel free to make those as i am just one guy imagining scenarios
+like everything in this project, feel free to make this more compelex
+
+
+"""
+def wake(cata, time, max_time): # decides whether or not the person will wake up and go to the park at a given time
+  if type == 1:
+    if time/max_time < 0.1: # This is based on a float/fraction, so the numbers 
