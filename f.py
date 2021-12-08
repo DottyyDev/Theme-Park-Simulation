cd6e78dd096f97ef81f8e11b7ea927ef386efe74
DottyyDev <91227484+DottyyDev@users.noreply.github.com>
2021-12-08 13:28:48 -0500

    updating things yes

--- f.py
+++ f.py
@@ -11,8 +11,8 @@ def fetch(list,place):
   return list[place]
 
 
-def wake(cata, time, max_time): # decides whether or not the person will wake up and go to the park at a given time
-  if type == 1:
+def wake(c, time, max_time): # decides whether or not the person will wake up and go to the park at a given time
+  if c == 1:
      # This is based on a float/fraction, so the numbers are between 0 and 1
       i = random.randint(1,3-4(time/max_time)) #once in a lifetimers are more likely to start early
       if i == 1:
@@ -20,9 +20,26 @@ def wake(cata, time, max_time): # decides whether or not the person will wake up
       else:
         return false
     
-  if type == 2:
+  elif c == 2:
      # This is based on a float/fraction, so the numbers are between 0 and 1
       i = random.randint(1,9-4(time/max_time)) # Every few yearers will come around noon, tending to be more early
+      if i == 1:
+        return true
+      else:
+        return false
+
+
+  elif c == 3:
+     # This is based on a float/fraction, so the numbers are between 0 and 1
+      i = random.randint(1,12-4(time/max_time)) # Year passers come a little earlier than noon since its still a bit a of a drive
+      if i == 1:
+        return true
+      else:
+        return false      
+      
+  if c == 4:
+     # This is based on a float/fraction, so the numbers are between 0 and 1
+      i = random.randint(1,16-4(time/max_time)) # Bloggers only come around prime time
       if i == 1:
         return true
       else:
